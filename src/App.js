cfd35984eaac58f3de7f3bd716cb6de8a0c2553e
github username <github email address>
2021-01-16 00:03:34 -0500

    changed file name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import SomeThing from './components/SomeThing';
 
 function App() {
   return (
     <div className="App">
       <h1>GoodFlix</h1>
+      <SomeThing/>
     </div>
   );
 }
---
+++ src/components/SomeThing.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SomeThing extends Component {
+  render(){
+    return(
+      <div>SomeThing</div>
+    )
+  }
+
+}
+
+export default SomeThing
